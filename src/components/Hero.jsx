733865ad93056bc34405a5cd22fe08991b10fe64
Bohdan Kondratskyi <bodakondrackij@gmail.com>
2026-02-15 09:48:50 +0100

    hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section id="hero">
+      <div>
+        <h1>MacBook Pro</h1>
+        <img src="/title.png" alt="macbook title" />
+      </div>
+
+      <video src="/videos/hero.mp4" autoPlay muted playsInline />
+
+      <button>Buy</button>
+
+      <p>From $1599 or $133/mo for 12 months</p>
+    </section>
+  );
+};
+
+export default Hero;
